9ec89034e2ff10d7a956af187d579c57227b4eff
thomroz <thomroz@fastmail.com>
2018-07-19 12:33:50 -0700

    initial commit of basic ec2 instance

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_key_pair" "tfkey" {
+  key_name   = "tfkey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCf2qAiVLLwmrkWBhYp1ACp6tLE04diFwGSKoxPA2AcpK0CCTgq8QbJJFEAEgPtxclmFxYg6xs8mSNq8z13fXCG4v0zw+mq3F+QdZxGY4RoaGS+K7MCQ2FzvuNP27wDPwfBFhyrO2JGuG83kAQ+eShaRVabpgSJadW3Rx38D3ZkzwfG7rCgusUHu8XFgyTygvwed/Um2dyXpz0xSjk/0k/KF4v4aK0SCRXVjp/Rh+KgUmy82tnIzy8Vxie0hKl2uCK/Ssuq4VYgDjI97LUMzhrHtVNTWFVL8sjK5VWY0+TxPNbnfVXkP9vIIqTRZmcxJEbVaZu7XHMJGnQN9ZtnV+or thomas@m700"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["76.93.151.189/32"]
+  }
+}
+
+resource "aws_instance" "tfexample" {
+  ami           = "ami-0ad99772"
+  instance_type = "t2.micro"
+
+  key_name = "tfkey"
+
+  security_groups = ["allow_ssh"]
+
+  tags {
+    Name = "ats-terraform-example"
+  }
+}
