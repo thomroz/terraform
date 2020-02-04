ba5e756df86a974ad2ba4256088d35982f1e899f
thomroz <thomroz@fastmail.com>
2020-02-04 11:46:30 -0800

    removed old keys; updated main.tf to TF 0.12 syntax; removed Route53 items

--- postgres/main.tf
+++ postgres/main.tf
@@ -5,39 +5,39 @@ provider "aws" {
 # create a keypair resource
 resource "aws_key_pair" "tfkey" {
   key_name   = "tfkey"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCf2qAiVLLwmrkWBhYp1ACp6tLE04diFwGSKoxPA2AcpK0CCTgq8QbJJFEAEgPtxclmFxYg6xs8mSNq8z13fXCG4v0zw+mq3F+QdZxGY4RoaGS+K7MCQ2FzvuNP27wDPwfBFhyrO2JGuG83kAQ+eShaRVabpgSJadW3Rx38D3ZkzwfG7rCgusUHu8XFgyTygvwed/Um2dyXpz0xSjk/0k/KF4v4aK0SCRXVjp/Rh+KgUmy82tnIzy8Vxie0hKl2uCK/Ssuq4VYgDjI97LUMzhrHtVNTWFVL8sjK5VWY0+TxPNbnfVXkP9vIIqTRZmcxJEbVaZu7XHMJGnQN9ZtnV+or thomas@m700"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYHdldcpQXANcd+4mbuLaz5LR/ZXPWWPLGqV5bqAWYrxaAiEa/gbKq3YzzPIEmlknIAz9Kjxrn4MkZn3PhaiL54LorYZbLLTB6wwUHRqTEg7v1qxR3CqfQsWDJdnYLPt1482oZ1uALfogea/9g07ElaAHauENYkzm/kyFWdXsSJUDnGHrCWngrp88GkH2fcKcabPH98DJmww32CZps2javPoWNRVTCT/hMwQGtOSO6B+YvbwBZCW3D6lujTo56hChxlnDHf9tjwfSGkww6X8c8kbUys8U4wh1YF94PUHn47BM3299p2TbQEx7Z+0dgWQ8ZJZVXBvFKbNhU47wo5RgL thomas@macair.local"
 }
 
 # create an EIP to attach to the new EC2 instance
 resource "aws_eip" "tfeip" {
-  instance = "${aws_instance.tfexample.id}"
+  instance = aws_instance.tfexample.id
   vpc      = true
 }
 
-# get info about the atssrv.com zone as a data object
-data "aws_route53_zone" "selected" {
-  name = "atssrv.com."
-}
+# # get info about the atssrv.com zone as a data object
+# data "aws_route53_zone" "selected" {
+#   name = "atssrv.com."
+# }
 
-# create an A-record and attach it to our EIP
-resource "aws_route53_record" "a_record" {
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "${data.aws_route53_zone.selected.name}"
-  type    = "A"
-  ttl     = "300"
+# # create an A-record and attach it to our EIP
+# resource "aws_route53_record" "a_record" {
+#   zone_id = "${data.aws_route53_zone.selected.zone_id}"
+#   name    = "${data.aws_route53_zone.selected.name}"
+#   type    = "A"
+#   ttl     = "300"
 
-  records = ["${aws_eip.tfeip.public_ip}"]
-}
+#   records = ["${aws_eip.tfeip.public_ip}"]
+# }
 
-# create a CNAME record and attach it to our EIP
-resource "aws_route53_record" "cname_record" {
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "www.${data.aws_route53_zone.selected.name}"
-  type    = "CNAME"
-  ttl     = "300"
+# # create a CNAME record and attach it to our EIP
+# resource "aws_route53_record" "cname_record" {
+#   zone_id = "${data.aws_route53_zone.selected.zone_id}"
+#   name    = "www.${data.aws_route53_zone.selected.name}"
+#   type    = "CNAME"
+#   ttl     = "300"
 
-  records = ["atssrv.com"]
-}
+#   records = ["atssrv.com"]
+# }
 
 # create a security group that allows SSH traffic inbound so we can connect to our EC2 instance
 resource "aws_security_group" "allow_ssh" {
@@ -54,13 +54,13 @@ resource "aws_security_group" "allow_ssh" {
 
 # create the EC2 instance
 resource "aws_instance" "tfexample" {
-  ami           = "ami-0ad99772"
+  ami           = "ami-a042f4d8"
   instance_type = "t2.micro"
 
   key_name        = "tfkey"
   security_groups = ["allow_ssh", "allow_postgres"]
 
-  tags {
+  tags = {
     Name = "atssrv-tf-instance"
   }
 }
@@ -82,12 +82,12 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "10.4"
+  engine_version       = "11.5"
   instance_class       = "db.t2.micro"
   name                 = "thomasdb"
   username             = "thomas"
   password             = "thomas123"
-  parameter_group_name = "default.postgres10"
+  parameter_group_name = "default.postgres11"
   apply_immediately    = "true"
   publicly_accessible  = "true"
 }
