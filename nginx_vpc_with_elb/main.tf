7c47d5fa010395f11f04637c8450ace561bc7776
thomroz <thomroz@fastmail.com>
2020-02-07 18:12:54 -0800

    vpc and subnet coded

--- nginx_vpc_with_elb/main.tf
+++ nginx_vpc_with_elb/main.tf
@@ -8,4 +8,40 @@ resource "aws_vpc" "nginx_vpc" {
   cidr_block = "172.16.0.0/16"
 }
 
+resource "aws_subnet" "nginx_public_sn_az_a" {
+  vpc_id     = aws_vpc.nginx_vpc.id
+  cidr_block = "172.16.1.0/24"
+
+  tags = {
+    Name = "nginx_public_sn_az_a"
+  }
+}
+
+resource "aws_subnet" "nginx_public_sn_az_b" {
+  vpc_id     = aws_vpc.nginx_vpc.id
+  cidr_block = "172.16.0/24"
+
+  tags = {
+    Name = "nginx_public_sn_az_b"
+  }
+}
+
+resource "aws_subnet" "nginx_private_sn_az_a" {
+  vpc_id     = aws_vpc.nginx_vpc.id
+  cidr_block = "172.16.0/24"
+
+  tags = {
+    Name = "nginx_private_sn_az_a"
+  }
+}
+
+resource "aws_subnet" "nginx_private_sn_az_b" {
+  vpc_id     = aws_vpc.nginx_vpc.id
+  cidr_block = "172.16.0/24"
+
+  tags = {
+    Name = "nginx_private_sn_az_b"
+  }
+}
+
 
