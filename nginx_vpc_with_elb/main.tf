232f4af6e0760c46f1bbd3fc9c8f28b1d5641fb9
thomroz <thomroz@fastmail.com>
2020-02-07 18:17:28 -0800

    tightened subnet creation details

--- nginx_vpc_with_elb/main.tf
+++ nginx_vpc_with_elb/main.tf
@@ -9,8 +9,10 @@ resource "aws_vpc" "nginx_vpc" {
 }
 
 resource "aws_subnet" "nginx_public_sn_az_a" {
-  vpc_id     = aws_vpc.nginx_vpc.id
-  cidr_block = "172.16.1.0/24"
+  vpc_id                  = aws_vpc.nginx_vpc.id
+  cidr_block              = "172.16.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "nginx_public_sn_az_a"
@@ -18,8 +20,10 @@ resource "aws_subnet" "nginx_public_sn_az_a" {
 }
 
 resource "aws_subnet" "nginx_public_sn_az_b" {
-  vpc_id     = aws_vpc.nginx_vpc.id
-  cidr_block = "172.16.0/24"
+  vpc_id                  = aws_vpc.nginx_vpc.id
+  cidr_block              = "172.16.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "nginx_public_sn_az_b"
@@ -27,8 +31,9 @@ resource "aws_subnet" "nginx_public_sn_az_b" {
 }
 
 resource "aws_subnet" "nginx_private_sn_az_a" {
-  vpc_id     = aws_vpc.nginx_vpc.id
-  cidr_block = "172.16.0/24"
+  vpc_id            = aws_vpc.nginx_vpc.id
+  cidr_block        = "172.16.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "nginx_private_sn_az_a"
@@ -36,8 +41,9 @@ resource "aws_subnet" "nginx_private_sn_az_a" {
 }
 
 resource "aws_subnet" "nginx_private_sn_az_b" {
-  vpc_id     = aws_vpc.nginx_vpc.id
-  cidr_block = "172.16.0/24"
+  vpc_id            = aws_vpc.nginx_vpc.id
+  cidr_block        = "172.16.0/24"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "nginx_private_sn_az_b"
