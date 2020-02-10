2a6e74ca590edd730e42546226816a1785c41c20
thomroz <thomroz@fastmail.com>
2020-02-10 08:25:14 -0800

    cleaned up unused dirs

--- demo/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "myexample"{
-  ami           = "ami-06d51e91cea0dac8d"
-  instance_type = "t2.micro"
-}
-
--- import_test/main.tf
+++
@@ -1,8 +0,0 @@
-
-
-provider "aws" {
-  region = "us-west-2"
-}
-resource "aws_instance" "tf_imported_ec2_a" {
-  # ...instance configuration...
-}
