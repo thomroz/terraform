4222ba7f32ad0c04a5a102127f666b50388a2b50
thomroz <thomroz@fastmail.com>
2020-02-10 08:37:14 -0800

    changed cidr_block

--- postgres/main.tf
+++ postgres/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "allow_postgres" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["76.93.151.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
